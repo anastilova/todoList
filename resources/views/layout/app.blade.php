a1a83335a9cc671741880de9a52e78dd970f4431
anastilova <90645056+anastilova@users.noreply.github.com>
2022-12-28 17:41:19 +0400

    Update app.blade.php

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="/assets/css/style.css" rel="stylesheet">
-    <link href="/assets/fontawesome-free-6.2.1-web/css/all.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" rel="stylesheet">
     <title>ToDO</title>
 </head>
 <body>
